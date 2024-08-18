7444af2cc9a1cc1950b5eb5627bcb7a8c8728e80
coderFeedForwardAlg <gaspersscottma@gmail.com>
2024-08-18 19:16:54 -0400

    add_post_file_edit

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use std::net::TcpListener;
 use std::net::TcpStream;
 use std::io::prelude::*;
-use std::fs; 
 
+use std::fs::File;
 use std::process::Command;
+
+use rocket::State;
 
 
 
@@ -27,13 +29,13 @@ use std::process::Command;
 // }
 
 
-// let mut dir = "./";
+
 #[get("/cmd/<command>/<args>")]
 fn comm(command: &str, args: &str) -> String {
     // if command == "cd" {
     //     dir = dir + args;
     //     return "moved dir";
-    // } 
+    // }
     let arr: Vec<&str> = args.split("+").collect();
     let p1 = Command::new(command)
         .current_dir("./to_run/src")
@@ -43,38 +45,50 @@ fn comm(command: &str, args: &str) -> String {
     // println!("status: {}", output.unwrap().status);
 
     // let res = output.unwrap()
-    
+
     let hello = String::from_utf8(p1.unwrap().stdout).unwrap(); //res.stdout;
     return hello;
 
 }
 
-#[get("/cmd/<command>/<args>")]
-fn comm2(command: &str, args: &str) -> String {
+
+#[post("/edit/<file>", format = "application/json", data = "<text>")]
+fn comm2(file: &str, text: &str) -> String {
+    // if command == "cd" {) -> String {
+    // println!("{} ", Dir::dir);
     // if command == "cd" {
     //     dir = dir + args;
     //     return "moved dir";
-    // } 
-    let arr: Vec<&str> = args.split("+").collect();
-    let p1 = Command::new(command)
+    // }
+    // let arr: Vec<&str> = args.split("+").collect();
+    /*let p1 = Command::new("echo")
         .current_dir("./to_run")
-        .args(arr)
-        .output();
+        .args([text, ">", file])
+        .output(); */
         // .expect("failed to execute process");
     // println!("status: {}", output.unwrap().status);
 
     // let res = output.unwrap()
-    
-    let hello = String::from_utf8(p1.unwrap().stdout).unwrap(); //res.stdout;
-    return hello;
+
+    //let hello = String::from_utf8(p1.unwrap().stdout).unwrap(); //res.stdout;
+
+    let mut file = std::fs::File::create(file);
+    file.expect("reason").write_all(text.as_bytes());
+    return "saved".to_string();
 
 }
 
+// why use a struct wit hone thing ??? fix
+struct Dir {
+    dir: String
+}
 
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![comm])
+    rocket::build().mount("/", routes![comm, comm2]).manage(Dir { dir: "hello".to_string() })
 }
+
+
 
 
